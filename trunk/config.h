4a9b92190e70cc89c510dadc8cdd7f4fa76aaba8
darwin.yuan <darwin.yuan@af261274-c94b-0410-88ce-a9c2d82df56d>
2008-04-26 17:22:58 +0000

    initial commit

---
+++ trunk/config.h
@@ -0,0 +1,185 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* 1234 = LIL_ENDIAN, 4321 = BIGENDIAN */
+#define BYTEORDER 1234
+
+/* Define if you don't use CxxTest namespace. */
+/* #undef CXXTEST_NO_NAMESPACE */
+
+/* Define if the std namespace is missing. */
+/* #undef CXXTEST_NO_STD_NS */
+
+/* Define to use built-in mini stl. */
+/* #undef CXXTEST_USE_MINI_STL */
+
+/* Define to turn debugging globally on. */
+/* #undef DEBUG */
+
+/* Define to 1 if you have the `bcopy' function. */
+#define HAVE_BCOPY 1
+
+/* Define if you have boost installed. */
+#define HAVE_BOOSTTEST 1
+
+/* Define to 1 if you have the <check.h> header file. */
+/* #undef HAVE_CHECK_H */
+
+/* Define if you have CppUnit installed. */
+/* #undef HAVE_CPPUNIT */
+
+/* Define to 1 if you have the <cwchar> header file. */
+#define HAVE_CWCHAR 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have libdl */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if you have the <limits> header file. */
+#define HAVE_LIMITS 1
+
+/* Define to 1 if you have the <machine/limits.h> header file. */
+/* #undef HAVE_MACHINE_LIMITS_H */
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mmap' function. */
+#define HAVE_MMAP 1
+
+/* define if the compiler supports Run-Time Type Identification */
+#define HAVE_RTTI 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the std::wcscmp function. */
+#define HAVE_STD_WCSCMP 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <values.h> header file. */
+#define HAVE_VALUES_H 1
+
+/* Define if you have wcscmp */
+#define HAVE_WCSCMP 1
+
+/* Define if you have the std::wstring type. */
+#define HAVE_WSTRING 1
+
+/* Define if you don't use ministl namespace. */
+/* #undef MINISTL_NO_NAMESPACE */
+
+/* Define if the std namespace is missing. */
+/* #undef MINISTL_NO_STD_NS */
+
+/* Define to disable exception support. */
+/* #undef MOCKPP_NO_EXCEPTIONS */
+
+/* Define if you don't use mockpp namespace. */
+/* #undef MOCKPP_NO_NAMESPACE */
+
+/* Define to disable rtti support. */
+/* #undef MOCKPP_NO_RTTI */
+
+/* Define if the std namespace is missing. */
+/* #undef MOCKPP_NO_STD_NS */
+
+/* Define if the std::numeric_limits templates are missing. */
+/* #undef MOCKPP_NO_STD_NUMERICLIMITS */
+
+/* Define if the compiler does not support the template hint. */
+/* #undef MOCKPP_NO_TEMPLATE_HINT */
+
+/* restricted use of "typedef typename". */
+/* #undef MOCKPP_NO_TYPENAME_FOR_STL_NS */
+
+# define MOCKPP_PACKAGE  PACKAGE
+
+
+/* Turn on support for weak template capabilities. */
+/* #undef MOCKPP_PTI_WEAKNESS */
+
+/* Define to compile for unicode environment. */
+/* #undef MOCKPP_UNICODE */
+
+#define MOCKPP_UNUSED(x) ((void)(x))
+
+
+/* Define to use Boost.Test as framework. */
+/* #undef MOCKPP_USE_BOOSTTEST */
+
+/* Define to use built-in cxxtest. */
+#define MOCKPP_USE_BUILTIN_CXXTEST 1
+
+/* Define to use cppunit as framework. */
+/* #undef MOCKPP_USE_CPPUNIT */
+
+/* Define if you have CxxTest installed. */
+#define MOCKPP_USE_CXXTEST 1
+
+/* Define to use built-in mini stl. */
+/* #undef MOCKPP_USE_MINI_STL */
+
+# define MOCKPP_VERSION  VERSION
+
+
+/* version as long int */
+#define MOCKPP_VERSION_NUMBER 0x00011004
+
+/* Name of package */
+#define PACKAGE "mockpp"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.16.4"
+
+/* whether byteorder is bigendian */
+/* #undef WORDS_BIGENDIAN */
